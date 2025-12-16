6a201ef1687b6869abfc73fcbdc3f9bede9ee2cd
MK <mahesyt@gmail.com>
2025-12-16 14:23:54 +0800

    fix(config): rename next.config.ts to next.config.mjs for next 14 support

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    /* config options here */
+    output: process.env.NEXT_EXPORT === "true" ? "export" : undefined,
+    images: {
+        unoptimized: true, // Required for static export
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'hwztchapter.dramaboxdb.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+            },
+            {
+                protocol: "https",
+                hostname: "*.googleusercontent.com",
+            },
+        ],
+    },
+};
+
+export default nextConfig;
